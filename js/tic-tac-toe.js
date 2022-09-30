bfd875c78220de76b923b5a7bb5c09899a82a1e8
Justin <Justin.nthai@gmail.com>
2022-09-29 17:15:30 -0700

    Remove data winner msg dummy text from html, Add input-error class in css to be used for inputs in js, Rearrange gameController and displayController module to avoid lexical error, Form logic to determine winning player or draw, check/clear form input, and clear the gameboard of marks on restart

--- js/tic-tac-toe.js
+++ js/tic-tac-toe.js
@@ -1,81 +1,18 @@
 (function() {
-  const gameController = (function() {
-    const gameBoard = []; // maybe find way to put nodelist tttcell into array with spread?
-    const winningSequences = [
-      [0, 1, 2], [3, 4, 5], [6, 7, 8],
-      [0, 3, 6], [1, 4, 7], [2, 5, 8],
-      [0, 4, 8], [2, 4, 6]
-    ];
-    const tttGrid = document.querySelector('[data-ttt-grid]');
-    const tttCell = document.querySelectorAll('[data-ttt-cell]');
-    let xTurn;
-
-    _init(); // make call start button and restart button here and clear the board of classes
-
-    function _init() {
-      tttCell.forEach(cell => cell.addEventListener('click', _controlTurn, { once : true }));
-      xTurn = true;
-      _indicateRoundHover();
-    }
-
-    function _render(cell, currentTurn) {
-      cell.classList.add(currentTurn);
-    }
-
-    function _controlTurn(e) {
-      const xMark = 'x-mark';
-      const oMark = 'o-mark';
-      const cell = e.target;  
-      let currentTurn = xTurn ? xMark : oMark;
-      _render(cell, currentTurn);
-      xTurn = !xTurn;
-      _indicateRoundHover();
-      if (_checkWin(currentTurn)) {
-        displayController.changeDisplay();
-      } 
-    }
-
-    function _indicateRoundHover() {
-      const xRound = 'x-turn';
-      const oRound = 'o-turn';
-      tttGrid.classList.remove(xRound);
-      tttGrid.classList.remove(oRound);
-      if (xTurn) {
-        tttGrid.classList.add(xRound);
-      } else {
-        tttGrid.classList.add(oRound);
-      }
-    }
-
-    function _checkWin(currentTurn) {
-      return winningSequences.some(sequence => {
-        return sequence.every(index => {
-          return tttCell[index].classList.contains(currentTurn);
-        });
-      });
-    }
-
-    return {
-      
-    };
-    
-  })()
-
   const displayController = (function() {
     const openingDisplay = document.querySelector('[data-opening-display]');
     const pvpBtn = document.querySelector('[data-pvp-btn]');
-    // const pvaBtn = document.querySelector('[data-pva-btn]');
+    // const pvaBtn = document.querySelector('[data-pva-btn]'); TBD
     const playerDisplay = document.querySelector('[data-player-display]');
     const winnerDisplay = document.querySelector('[data-winner-display]');
     const startBtn = document.querySelector('[data-start-btn]');
     const restartBtn = document.querySelector('[data-restart-btn]');
-    const winnerMsg = document.querySelector('[data-winner-msg]');
 
     _init();
 
     function _init() {
       pvpBtn.addEventListener('click', _transitionPlayerDisplay);
-      // pvaBtn.addEventListener('click', _transitionPlayerDisplay);
+      // pvaBtn.addEventListener('click', _transitionPlayerDisplay); TBD
       startBtn.addEventListener('click', _transitionOpeningDisplay);
       restartBtn.addEventListener('click', () => { 
         _transitionWinnerDisplay();
@@ -86,10 +23,12 @@
     function _render(display) {
       switch (display) {
         case 'opening':
-          if (openingDisplay.classList.contains('hide')) {
+          if (Object.keys(playerController.players).length === 2) { // possible scope issue here in the playercontroller module, players is empty 
+            if (openingDisplay.classList.contains('hide')) {
             openingDisplay.classList.remove('hide');
-          } else {
+            } else {
             openingDisplay.classList.add('hide');
+            }
           }
           break;
         case 'player':
@@ -129,6 +68,7 @@
     }
 
     return {
+      pvpBtn : pvpBtn,
       startBtn : startBtn,
       restartBtn : restartBtn,
       changeDisplay : changeDisplay
@@ -136,25 +76,119 @@
 
   })()
 
-  const playerController = (function() { // create factory within module since there are other related functions.., check for empty input values on click
-    const players = {};                  // you can keep the object keys, but just remove the properties on restart
+const gameController = (function() {
+    const winningSequences = [
+      [0, 1, 2], [3, 4, 5], [6, 7, 8],
+      [0, 3, 6], [1, 4, 7], [2, 5, 8],
+      [0, 4, 8], [2, 4, 6]
+    ];
+    const tttGrid = document.querySelector('[data-ttt-grid]');
+    const tttCell = document.querySelectorAll('[data-ttt-cell]');
+    const winnerMsg = document.querySelector('[data-winner-msg]');
+    const xMark = 'x-mark';
+    const oMark = 'o-mark';
+    let xTurn;
+    
+
+    _init();
+
+    function _init() {
+      tttCell.forEach(cell => {
+        cell.addEventListener('click', _controlTurn, { once : true })
+      });
+      xTurn = true;
+      _indicateRoundHover();
+      displayController.restartBtn.addEventListener('click', _wipeBoard)
+    }
+
+    function _render(cell, currentTurn) {
+      cell.classList.add(currentTurn);
+    }
+
+    function _controlTurn(e) {
+      const cell = e.target;  
+      let currentTurn = xTurn ? xMark : oMark;
+      _render(cell, currentTurn);
+      if (_checkWin(currentTurn)) {
+        winnerMsg.innerText = `${xTurn ? playerController.players.player1 : playerController.players.player2} Dominates!`;
+        displayController.changeDisplay();
+      } else if ([...tttCell].every(cell => cell.classList.contains(xMark) || cell.classList.contains(oMark))) {
+        winnerMsg.innerText = `It's a Stalemate!`
+        displayController.changeDisplay();
+      }
+      xTurn = !xTurn;
+      _indicateRoundHover();
+    }
+
+    function _indicateRoundHover() {
+      const xRound = 'x-turn';
+      const oRound = 'o-turn';
+      tttGrid.classList.remove(xRound);
+      tttGrid.classList.remove(oRound);
+      if (xTurn) {
+        tttGrid.classList.add(xRound);
+      } else {
+        tttGrid.classList.add(oRound);
+      }
+    }
+
+    function _checkWin(currentTurn) {
+      return winningSequences.some(sequence => {
+        return sequence.every(index => {
+          return tttCell[index].classList.contains(currentTurn);
+        });
+      });
+    }
+
+    function _wipeBoard() {
+      tttCell.forEach(cell => {
+        cell.classList.remove(xMark);
+        cell.classList.remove(oMark);
+        cell.removeEventListener('click', _controlTurn);
+      })
+    }
+
+  })()
+
+  const playerController = (function() {
+    let players = {};
     const player1 = document.querySelector('[data-player-1]');
     const player2 = document.querySelector('[data-player-2]');
 
     _init();
 
     function _init() {
-      displayController.startBtn.addEventListener('click', _addPlayers);
+      displayController.pvpBtn.addEventListener('click', _wipePlayers);
+      player1.addEventListener('keyup', _checkInput);
+      player2.addEventListener('keyup', _checkInput);
+      displayController.startBtn.addEventListener('click', _checkInput);
       displayController.restartBtn.addEventListener('click', _wipePlayers);
     }
 
+    function _checkInput() {
+      if (player1.value === '') {
+        player1.classList.add('input-error');
+      } else {
+        player1.classList.remove('input-error');
+      }
+      if (player2.value === '') {
+        player2.classList.add('input-error');
+      } else {
+        player2.classList.remove('input-error');
+      }
+      _addPlayers();
+    }
+
     function _addPlayers() {
+      players = {};
       players.player1 = player1.value;
       players.player2 = player2.value;
     }
 
     function _wipePlayers() {
-      
+      players = {};
+      player1.value = '';
+      player2.value = '';
     }
 
     return {
